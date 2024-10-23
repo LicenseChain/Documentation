6ab95a2e0bebb0f8098adf1613f27a606e190350
CryptoJoma <jomasacadev@gmail.com>
2024-10-22 20:36:05 -0600

    v0.0.1

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'LicenseChain',
+			logo: {
+				src: './src/assets/logo.png',
+				replacesTitle: true,
+      },
+			social: {
+				github: 'https://github.com/LicenseChain',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
